9efbfafbe39b8b09b306b2b08849aa53c34b0a00
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-10-25 22:33:13 +0200

    do milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ Mostriamo in pagina
  una card per ciascun attore, con grafica a piacimento! */
 import axios from "axios";
 import { useState } from "react";
+import { useEffect } from "react";
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
       })
 
   }
+  useEffect(fetchData, []);
   return (
     <>
       <header>
@@ -51,10 +53,10 @@ function App() {
       <main>
         <section id="characters">
           <div className="container">
-            <div className="row g-5">
+            <div className="row g-5 align-items-stretch">
               {characters.map((character, i) =>
                 <div className="col-12 col-sm-6 col-md-4" key={character.id}>
-                  <div className="card h-100">
+                  <div className="card h-100 d-flex flex-column">
                     <img className="card-img-top img-fluid" src={character.image} alt="" />
                     <div className="card-body">
                       <h3>{character.name}</h3>
