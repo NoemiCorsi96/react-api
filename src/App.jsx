5286a6800ca501647f274d69b9add8c20bccf7df
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-10-25 22:53:42 +0200

    Bonus1: added list of actor cards on the page

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,18 @@ MILESTONE 3
 
 Mostriamo in pagina
  una card per ciascun attore, con grafica a piacimento! */
+
+
+
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 
 
 function App() {
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
+  const [actors, setActors] = useState([]);
+
   function fetchData() {
     console.log('Fetch the data');
     axios.get('https://lanciweb.github.io/demo/api/actresses/')
@@ -38,20 +43,33 @@ function App() {
       })
 
   }
-  useEffect(fetchData, []);
+
+  function fetchActors() {
+    axios.get('https://lanciweb.github.io/demo/api/actors/')
+      .then(res => {
+        console.log(res.data);
+        setActors(res.data);
+      })
+      .catch(err => {
+        console.error(err.message)
+      })
+  }
+
+  useEffect(() => { fetchData(); fetchActors(); }, []);
+
   return (
     <>
       <header>
         <div className="container d-flex justify-content-between align-items-center">
           <div className="logo">
-            <strong>Attrici</strong>
+            <h1><strong>Attrici&Attori</strong></h1>
           </div>
 
-          <button className="btn btn-dark" onClick={fetchData}>Click</button>
+          {/*<button className="btn btn-dark" onClick={fetchData}>Click</button>*/}
         </div>
       </header>
       <main>
-        <section id="characters">
+        <section className="actresses mt-5 mb-5">
           <div className="container">
             <div className="row g-5 align-items-stretch">
               {characters.map((character, i) =>
@@ -64,6 +82,27 @@ function App() {
                       <h5>{character.nationality}</h5>
                       <h5>{character.biography}</h5>
                       <h5 className="awards text-warning">{character.awards}</h5>
+
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </section>
+        <section id="actors">
+          <div className="container">
+            <div className="row g-5 align-items-stretch">
+              {actors.map((actors, i) =>
+                <div className="col-12 col-sm-6 col-md-4" key={actors.id}>
+                  <div className="card h-100 d-flex flex-column">
+                    <img className="card-img-top img-fluid" src={actors.image} alt="" />
+                    <div className="card-body">
+                      <h3>{actors.name}</h3>
+                      <h5>{actors.birth_year}</h5>
+                      <h5>{actors.nationality}</h5>
+                      <h5>{actors.biography}</h5>
+                      <h5 className="awards text-warning">{actors.awards}</h5>
 
                     </div>
                   </div>
